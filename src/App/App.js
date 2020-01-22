c59b085c9de85c7488fac1a12571cb80c6287d99
alesha reed <aleshak83@gmail.com>
2020-01-22 16:48:13 -0600

    added authData Auth component

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConnection from '../helpers/data/connection';
+
+import Auth from '../components/pages/Auth/Auth';
 import './App.scss';
 
+firebaseConnection();
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  };
+
+  componentDidMount() {
+    this.removeListner = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
        <button className="btn button-outline">App</button>
+       < Auth/>
       </div>
     );
   }
